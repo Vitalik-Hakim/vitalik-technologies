da5d7165bbf8d5458ab22f66001742dbb482e3d1
Vitalik Hakim <73163003+Vitalik-Hakim@users.noreply.github.com>
2024-10-15 12:21:19 +0000

    Update header.tsx

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
           <div className="flex flex-1 items-center">
             <Logo />
             <h1
-              className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.indigo.200),theme(colors.gray.50),theme(colors.indigo.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle text-4xl font-semibold text-transparent md:text-5xl"
+              className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.indigo.200),theme(colors.gray.50),theme(colors.indigo.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text font-nacelle text-3xl font-semibold text-transparent md:text-3xl"
               data-aos="fade-up"
             >
               Vitalik Technologies
