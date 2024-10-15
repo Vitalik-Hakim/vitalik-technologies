94124cd6a34ae0a9ae1219f9f392b4c87c8c967f
Vitalik Hakim <73163003+Vitalik-Hakim@users.noreply.github.com>
2024-10-15 11:42:53 +0000

    Update page.tsx

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: "Home - Open PRO",
-  description: "Page description",
+  title: "Vitalik Technologies | Versatile digital agency",
+  description: "At Vitalik Technologies, we are a versatile digital agency committed to creating tailored solutions that drive growth and elevate your brand. Our expert team specializes in web development, digital marketing, branding, and design, ensuring your business reaches its full potential in the digital landscape. We focus on innovation, adaptability, and results, offering a range of services that are both flexible and scalable to meet your unique needs. Whether you're a startup or an established business, we provide strategic support that aligns with your goals, helping you thrive in an ever-changing market.",
 };
 
 import PageIllustration from "@/components/page-illustration";
