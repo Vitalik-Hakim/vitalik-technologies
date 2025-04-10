d9ff935e410a4a2764488efe103e8423863d2834
Vitalik Hakim <73163003+Vitalik-Hakim@users.noreply.github.com>
2025-04-10 02:36:47 +0000

    Update hero-home.tsx

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -27,7 +27,7 @@ At Vitalik Technologies, we specialize in creating innovative digital solutions 
                 <div data-aos="fade-up" data-aos-delay={400}>
                   <a
                     className="btn group mb-4 w-full bg-gradient-to-t from-indigo-600 to-indigo-500 bg-[length:100%_100%] bg-[bottom] text-white shadow-[inset_0px_1px_0px_0px_theme(colors.white/.16)] hover:bg-[length:100%_150%] sm:mb-0 sm:w-auto"
-                    href="mailto:vitalikhakim@gmail.com?subject=Vitalik%20a%Technologies%20a%Meeting"
+                    href="mailto:?subject=Vitalik%20a%Technologies%20a%Meeting"
                   >
                     <span className="relative inline-flex items-center">
                       Start Your Project
@@ -40,7 +40,7 @@ At Vitalik Technologies, we specialize in creating innovative digital solutions 
                 <div data-aos="fade-up" data-aos-delay={600}>
                   <a
                     className="btn relative w-full bg-gradient-to-b from-gray-800 to-gray-800/60 bg-[length:100%_100%] bg-[bottom] text-gray-300 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,theme(colors.gray.800),theme(colors.gray.700),theme(colors.gray.800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-[length:100%_150%] sm:ml-4 sm:w-auto"
-                    href="mailto:vitalikhakim@gmail.com?subject=Vitalik%20a%Technologies%20a%Meeting"
+                    href="mailto:subject=Vitalik%20a%Technologies%20a%Meeting"
                   >
                     Schedule a Meeting
                   </a>
